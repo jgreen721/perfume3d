aee8fc80c52f4bc6174d7bf6854319a0227a891a
Justin Green <justingreen@Justins-MacBook-Air.local>
2023-12-19 09:43:29 -0500

    save

--- src/views/Solution2/Overlay.jsx
+++ src/views/Solution2/Overlay.jsx
@@ -5,12 +5,12 @@ const Overlay = ({setSolution1}) => {
   return (
     <div className="overlay">
         <div className="perfume-card">
-            <h1 className="fancy my-2"> Gabrielle Essence Eau De Parfum</h1>
+            <h1 className="fancy my-2 green"> Gabrielle Essence Eau De Parfum</h1>
             <h5>  A floral, solar and voluptuous interpretation composed by Olivier Polge, 
   Perfumer-Creator for the House of CHANEL.</h5>
   <div className="prices-row my-2">
-      <h2 className="fancy">  $149.99 </h2>
-   <h2> $169.99</h2>
+      <h2 className="fancy green">  $149.99 </h2>
+   <h2 className="non-sale-price"> $169.99</h2>
   </div>
   <button className="btn primary-btn">
       <img className="cart-img" src="./images/icon-cart.svg" alt="" />
